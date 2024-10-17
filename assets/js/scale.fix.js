30e2d7925028d9a34a53536f9f03bec65381f555
jacobfh1 <jacobfh95@gmail.com>
2024-10-17 16:00:30 +0200

    general update

--- assets/js/scale.fix.js
+++ assets/js/scale.fix.js
@@ -1,34 +1,26 @@
-// js/scale.fix.js
 (function(document) {
-    const metas = document.getElementsByTagName('meta');
-
-    // Change the content of the viewport meta tag
-    function changeViewportContent(content) {
-        for (let i = 0; i < metas.length; i++) {
-            if (metas[i].name === "viewport") {
-                metas[i].content = content;
+    var metas = document.getElementsByTagName('meta'),
+        changeViewportContent = function(content) {
+            for (var i = 0; i < metas.length; i++) {
+                if (metas[i].name == "viewport") {
+                    metas[i].content = content;
+                }
             }
-        }
-    }
+        },
+        initialize = function() {
+            changeViewportContent("width=device-width, minimum-scale=1.0, maximum-scale=1.0");
+        },
+        gestureStart = function() {
+            changeViewportContent("width=device-width, minimum-scale=0.25, maximum-scale=1.6");
+        },
+        gestureEnd = function() {
+            initialize();
+        };
 
-    // Initialize default viewport settings
-    function initialize() {
-        changeViewportContent("width=device-width, minimum-scale=1.0, maximum-scale=1.0");
-    }
 
-    // Adjust viewport during touch gestures (start)
-    function gestureStart() {
-        changeViewportContent("width=device-width, minimum-scale=0.25, maximum-scale=1.6");
-    }
-
-    // Reset viewport after touch gestures (end)
-    function gestureEnd() {
-        initialize();
-    }
-
-    // Apply settings for iPhone devices
     if (navigator.userAgent.match(/iPhone/i)) {
         initialize();
+
         document.addEventListener("touchstart", gestureStart, false);
         document.addEventListener("touchend", gestureEnd, false);
     }
